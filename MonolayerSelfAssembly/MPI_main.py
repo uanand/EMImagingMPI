c275ec2f701cae1de6c5cb4734ef26d6c55e1a02
uanand <utkarsh.aanand@gmail.com>
2016-11-23 00:50:31 +0800

    Print framewise occurence list in outputDir as data file.

--- MonolayerSelfAssembly/MPI_main.py
+++ MonolayerSelfAssembly/MPI_main.py
@@ -53,73 +53,73 @@ size = comm.Get_size()
 #########
 # PART 1
 #########
-#if (rank==0):
-    #fp = fileIO.createH5(outputFile)
+if (rank==0):
+    fp = fileIO.createH5(outputFile)
     
-    #[gImgRawStack,row,col,numFrames] = fileIO.readAVI(inputFile)
-    ##[gImgRawStack,row,col,numFrames] = fileIO.readImageSequence(folder,frameList=range(1,134))
-    ##[gImgRawStack,row,col,numFrames] = fileIO.readDM4Sequence(folder)
+    [gImgRawStack,row,col,numFrames] = fileIO.readAVI(inputFile)
+    #[gImgRawStack,row,col,numFrames] = fileIO.readImageSequence(folder,frameList=range(1,134))
+    #[gImgRawStack,row,col,numFrames] = fileIO.readDM4Sequence(folder)
     
-    #frameList = range(1,numFrames+1)
-    #for frame in frameList:
-        #fileIO.writeH5Dataset(fp,'/dataProcessing/gImgRawStack/'+str(frame).zfill(zfillVal),gImgRawStack[:,:,frame-1])
+    frameList = range(1,numFrames+1)
+    for frame in frameList:
+        fileIO.writeH5Dataset(fp,'/dataProcessing/gImgRawStack/'+str(frame).zfill(zfillVal),gImgRawStack[:,:,frame-1])
         
-    #fp.attrs['inputFile'] = inputFile
-    #fp.attrs['outputFile'] = outputFile
-    #fp.attrs['inputDir'] = inputDir
-    #fp.attrs['outputDir'] = outputDir
-    #fp.attrs['pixInNM'] = pixInNM
-    #fp.attrs['pixInAngstrom'] = pixInNM*10
-    #fp.attrs['fps'] = fps
-    #fp.attrs['microscope'] = microscope
-    #fp.attrs['camera'] = camera
-    #fp.attrs['owner'] = owner
-    #fp.attrs['row'] = row
-    #fp.attrs['col'] = col
-    #fp.attrs['numFrames'] = numFrames
-    #fp.attrs['frameList'] = range(1,numFrames+1)
-    #fp.attrs['zfillVal'] = zfillVal
+    fp.attrs['inputFile'] = inputFile
+    fp.attrs['outputFile'] = outputFile
+    fp.attrs['inputDir'] = inputDir
+    fp.attrs['outputDir'] = outputDir
+    fp.attrs['pixInNM'] = pixInNM
+    fp.attrs['pixInAngstrom'] = pixInNM*10
+    fp.attrs['fps'] = fps
+    fp.attrs['microscope'] = microscope
+    fp.attrs['camera'] = camera
+    fp.attrs['owner'] = owner
+    fp.attrs['row'] = row
+    fp.attrs['col'] = col
+    fp.attrs['numFrames'] = numFrames
+    fp.attrs['frameList'] = range(1,numFrames+1)
+    fp.attrs['zfillVal'] = zfillVal
     
-    #fileIO.mkdirs(outputDir)
-    #fileIO.saveImageSequence(gImgRawStack,outputDir+'/dataProcessing/gImgRawStack')
+    fileIO.mkdirs(outputDir)
+    fileIO.saveImageSequence(gImgRawStack,outputDir+'/dataProcessing/gImgRawStack')
     
-    #del gImgRawStack
-    #fp.flush(), fp.close()
-    #gc.collect()
-#comm.Barrier()
+    del gImgRawStack
+    fp.flush(), fp.close()
+    gc.collect()
+comm.Barrier()
 
 #########
 # PART 2
 #########
-#if (rank==0):
-    #print "Inverting the image and performing background subtraction"
-#invertFlag=True
-#bgSubFlag=True; bgSubSigmaTHT=2; radiusTHT=15
+if (rank==0):
+    print "Inverting the image and performing background subtraction"
+invertFlag=True
+bgSubFlag=True; bgSubSigmaTHT=2; radiusTHT=15
 
-#if (rank==0):
-    #fp = h5py.File(outputFile, 'r+')
-#else:
-    #fp = h5py.File(outputFile, 'r')
-#[row,col,numFrames,frameList] = misc.getVitals(fp)
-#procFrameList = numpy.array_split(frameList,size)
+if (rank==0):
+    fp = h5py.File(outputFile, 'r+')
+else:
+    fp = h5py.File(outputFile, 'r')
+[row,col,numFrames,frameList] = misc.getVitals(fp)
+procFrameList = numpy.array_split(frameList,size)
 
-#for frame in procFrameList[rank]:
-    #gImgProc = fp['/dataProcessing/gImgRawStack/'+str(frame).zfill(zfillVal)].value
-    #if (invertFlag==True):
-        #gImgProc = imageProcess.invertImage(gImgProc)
-    #if (bgSubFlag==True):
-        #gImgProc = imageProcess.subtractBackground(gImgProc, sigma=bgSubSigmaTHT, radius=radiusTHT)
-    #cv2.imwrite(outputDir+'/dataProcessing/processedStack/'+str(frame).zfill(zfillVal)+'.png',gImgProc)
+for frame in procFrameList[rank]:
+    gImgProc = fp['/dataProcessing/gImgRawStack/'+str(frame).zfill(zfillVal)].value
+    if (invertFlag==True):
+        gImgProc = imageProcess.invertImage(gImgProc)
+    if (bgSubFlag==True):
+        gImgProc = imageProcess.subtractBackground(gImgProc, sigma=bgSubSigmaTHT, radius=radiusTHT)
+    cv2.imwrite(outputDir+'/dataProcessing/processedStack/'+str(frame).zfill(zfillVal)+'.png',gImgProc)
 
-#comm.Barrier()
+comm.Barrier()
     
-#if (rank==0):
-    #for frame in frameList:
-        #gImgProc = cv2.imread(outputDir+'/dataProcessing/processedStack/'+str(frame).zfill(zfillVal)+'.png',0)
-        #fileIO.writeH5Dataset(fp,'/dataProcessing/processedStack/'+str(frame).zfill(zfillVal),gImgProc)
+if (rank==0):
+    for frame in frameList:
+        gImgProc = cv2.imread(outputDir+'/dataProcessing/processedStack/'+str(frame).zfill(zfillVal)+'.png',0)
+        fileIO.writeH5Dataset(fp,'/dataProcessing/processedStack/'+str(frame).zfill(zfillVal),gImgProc)
         
-#fp.flush(), fp.close()
-#comm.Barrier()
+fp.flush(), fp.close()
+comm.Barrier()
 #######################################################################
 
 
@@ -127,70 +127,70 @@ size = comm.Get_size()
 #######################################################################
 # IMAGE SEGMENTATION
 #######################################################################
-#if (rank==0):
-    #print "Performing segmentation for all the frames"
+if (rank==0):
+    print "Performing segmentation for all the frames"
     
-#fp = h5py.File(outputFile, 'r')
-#[row,col,numFrames,frameList] = misc.getVitals(fp)
-#procFrameList = numpy.array_split(frameList,size)
+fp = h5py.File(outputFile, 'r')
+[row,col,numFrames,frameList] = misc.getVitals(fp)
+procFrameList = numpy.array_split(frameList,size)
 
-#areaRange = numpy.array([60,500], dtype='float64')
-#circularityRange = numpy.array([0.85,1], dtype='float64')
-#sigma = 1
+areaRange = numpy.array([60,500], dtype='float64')
+circularityRange = numpy.array([0.85,1], dtype='float64')
+sigma = 1
 
-#for frame in procFrameList[rank]:
-    #gImgRaw = fp['/dataProcessing/gImgRawStack/'+str(frame).zfill(zfillVal)].value
-    #gImgNorm = imageProcess.normalize(gImgRaw,min=0,max=230)
-    #gImgProc = fp['/dataProcessing/processedStack/'+str(frame).zfill(zfillVal)].value
-    #bImgKapur = gImgProc>=myCythonFunc.threshold_kapur(gImgProc.flatten())
+for frame in procFrameList[rank]:
+    gImgRaw = fp['/dataProcessing/gImgRawStack/'+str(frame).zfill(zfillVal)].value
+    gImgNorm = imageProcess.normalize(gImgRaw,min=0,max=230)
+    gImgProc = fp['/dataProcessing/processedStack/'+str(frame).zfill(zfillVal)].value
+    bImgKapur = gImgProc>=myCythonFunc.threshold_kapur(gImgProc.flatten())
     
-    #gImgInv = 255-gImgRaw
-    #gImgBlur = ndimage.gaussian_filter(gImgInv, sigma=sigma)
-    #bImgAdaptive = cv2.adaptiveThreshold(gImgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 0).astype('bool')
+    gImgInv = 255-gImgRaw
+    gImgBlur = ndimage.gaussian_filter(gImgInv, sigma=sigma)
+    bImgAdaptive = cv2.adaptiveThreshold(gImgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 0).astype('bool')
     
-    #bImg = numpy.logical_and(bImgKapur,bImgAdaptive)
-    #bImg = imageProcess.fillHoles(bImg)
-    #bImg = myCythonFunc.removeBoundaryParticles(bImg.astype('uint8'))
-    #bImg = myCythonFunc.areaThreshold(bImg.astype('uint8'), areaRange=areaRange)
-    #bImg = myCythonFunc.circularThreshold(bImg.astype('uint8'), circularityRange=circularityRange)
-    #bImg = imageProcess.convexHull(bImg)
+    bImg = numpy.logical_and(bImgKapur,bImgAdaptive)
+    bImg = imageProcess.fillHoles(bImg)
+    bImg = myCythonFunc.removeBoundaryParticles(bImg.astype('uint8'))
+    bImg = myCythonFunc.areaThreshold(bImg.astype('uint8'), areaRange=areaRange)
+    bImg = myCythonFunc.circularThreshold(bImg.astype('uint8'), circularityRange=circularityRange)
+    bImg = imageProcess.convexHull(bImg)
     
-    #bImgBdry = imageProcess.normalize(imageProcess.boundary(bImg))
-    #finalImage = numpy.column_stack((numpy.maximum(gImgNorm,bImgBdry), gImgNorm))
-    #cv2.imwrite(outputDir+'/segmentation/result/'+str(frame).zfill(zfillVal)+'.png', finalImage)
-#fp.flush(), fp.close()
-#comm.Barrier()
+    bImgBdry = imageProcess.normalize(imageProcess.boundary(bImg))
+    finalImage = numpy.column_stack((numpy.maximum(gImgNorm,bImgBdry), gImgNorm))
+    cv2.imwrite(outputDir+'/segmentation/result/'+str(frame).zfill(zfillVal)+'.png', finalImage)
+fp.flush(), fp.close()
+comm.Barrier()
 #######################################################################
 
 
 #######################################################################
 # CREATE BINARY IMAGES INTO HDF5 FILE
 #######################################################################
-#if (rank==0):
-    #print "Creating binary images and writing into h5 file"
+if (rank==0):
+    print "Creating binary images and writing into h5 file"
     
-#if (rank==0):
-    #fp = h5py.File(outputFile, 'r+')
-#else:
-    #fp = h5py.File(outputFile, 'r')
-#[row,col,numFrames,frameList] = misc.getVitals(fp)
-#procFrameList = numpy.array_split(frameList,size)
+if (rank==0):
+    fp = h5py.File(outputFile, 'r+')
+else:
+    fp = h5py.File(outputFile, 'r')
+[row,col,numFrames,frameList] = misc.getVitals(fp)
+procFrameList = numpy.array_split(frameList,size)
 
-#for frame in procFrameList[rank]:
-    #bImg = cv2.imread(outputDir+'/segmentation/result/'+str(frame).zfill(zfillVal)+'.png',0)[0:row,0:col]
-    #bImg = bImg==255
-    #bImg = imageProcess.fillHoles(bImg)
-    #numpy.save(outputDir+'/segmentation/result/'+str(frame).zfill(zfillVal)+'.npy', bImg)
+for frame in procFrameList[rank]:
+    bImg = cv2.imread(outputDir+'/segmentation/result/'+str(frame).zfill(zfillVal)+'.png',0)[0:row,0:col]
+    bImg = bImg==255
+    bImg = imageProcess.fillHoles(bImg)
+    numpy.save(outputDir+'/segmentation/result/'+str(frame).zfill(zfillVal)+'.npy', bImg)
    
-#comm.barrier()
-#if (rank==0):
-    #for frame in frameList:
-        #bImg = numpy.load(outputDir+'/segmentation/result/'+str(frame).zfill(zfillVal)+'.npy')
-        #fileIO.writeH5Dataset(fp,'/segmentation/bImgStack/'+str(frame).zfill(zfillVal),bImg)
-        #fileIO.delete(outputDir+'/segmentation/result/'+str(frame).zfill(zfillVal)+'.npy')
+comm.barrier()
+if (rank==0):
+    for frame in frameList:
+        bImg = numpy.load(outputDir+'/segmentation/result/'+str(frame).zfill(zfillVal)+'.npy')
+        fileIO.writeH5Dataset(fp,'/segmentation/bImgStack/'+str(frame).zfill(zfillVal),bImg)
+        fileIO.delete(outputDir+'/segmentation/result/'+str(frame).zfill(zfillVal)+'.npy')
         
-#fp.flush(), fp.close()
-#comm.Barrier()
+fp.flush(), fp.close()
+comm.Barrier()
 #######################################################################
 
 
@@ -207,7 +207,7 @@ if (rank==0):
     [row,col,numFrames,frameList] = misc.getVitals(fp)
     maxID, occurenceFrameList = tracking.labelParticles(fp, centerDispRange=centerDispRange, perAreaChangeRange=perAreaChangeRange, missFramesTh=missFramesTh, structure=structure)
     fp.attrs['particleList'] = range(1,maxID+1)
-    numpy.savetxt('frameOccurenceList.dat',numpy.column_stack((fp.attrs['particleList'],occurenceFrameList)),fmt='%d')
+    numpy.savetxt(outputDir+'/frameOccurenceList.dat',numpy.column_stack((fp.attrs['particleList'],occurenceFrameList)),fmt='%d')
     fp.flush(), fp.close()
 comm.Barrier()
 
