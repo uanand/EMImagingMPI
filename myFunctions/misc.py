c048e3565c0954784156d9ccaa9e6eb14ff97fae
uanand <utkarsh.aanand@gmail.com>
2016-12-06 11:04:05 +0800

    Implmenting function to find out the seed point of different NPs.

--- myFunctions/misc.py
+++ myFunctions/misc.py
@@ -5,6 +5,8 @@ import imageio
 import h5py
 import imageProcess
 
+import matplotlib.pyplot as plt
+
 ##############################################################
 # GET SOME KEY INFROMATION FROM HDF5 FILE POINTER
 ##############################################################
@@ -16,9 +18,41 @@ def getVitals(fp):
 
 
 ##############################################################
-#
+# GECTH() LIKE FUNCTION
 ##############################################################
 def wait():
     raw_input()
     return 0
+##############################################################
+
+
+##############################################################
+# FIND SEED POINTS FOR ALL REGIONS OF THE SEGMENTED IMAGE
+##############################################################
+def findLocalMinima(bImg,gImg,size=2):
+    row,col = gImg.shape
+    labelImg = numpy.zeros([row,col], dtype='uint16')
+    seedList = []
+    R,C = numpy.where(bImg==True)
+    for r,c in zip(R,C):
+        if (r>=size and c>=size and r<row-size and c<=col-size):
+            #print r,c
+            r0,c0,value0 = r,c,gImg[r,c]
+            while (1):
+                flag = 0
+                bImgT = bImg[r0-size:r0+size+1,c0-size:c0+size+1]
+                gImgT = gImg[r0-size:r0+size+1,c0-size:c0+size+1]
+                gImgT = gImgT*bImgT
+                gImgT_max = numpy.max(gImgT)
+                if (value0 < gImgT_max):
+                    r1,c1 = numpy.where(gImgT==gImgT_max)
+                    r1,c1 = r1[0],c1[0]
+                    r0,c0,value0 = r0-size+r1,c0-size+c1,gImgT_max
+                    flag = 1
+                if (flag == 0):
+                    if ([r0,c0] not in seedList):
+                        seedList.append([r0,c0])
+                    labelImg[r,c] = seedList.index([r0,c0])+1
+                    break
+    return labelImg,seedList
 ##############################################################
